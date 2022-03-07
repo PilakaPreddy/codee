d5330925101c105519e85a426f17340809729861
coder19 <89542999+PilakaPreddy@users.noreply.github.com>
2022-03-07 22:06:37 +0530

    Add files via upload

---
+++ circleareaandcircum.java
@@ -0,0 +1,22 @@
+package prcode;
+
+	import java.util.Scanner;
+	class circleareaandcircum
+	{
+	   static Scanner s = new Scanner(System.in);
+	   public static void main(String args[]){
+		System.out.print("Enter the radius: ");
+	      /*We are storing the entered radius in double
+	       * because a user can enter radius in decimals
+	       */
+	      double radius = s.nextDouble();
+	      //Area = PI*radius*radius
+	      double area = Math.PI * (radius * radius);
+	      System.out.println("The area of circle is: " + area);
+	      //Circumference = 2*PI*radius
+	      double circumference= Math.PI * 2*radius;
+	      System.out.println( "The circumference of the circle is:"+circumference) ;
+
+	}
+
+}
